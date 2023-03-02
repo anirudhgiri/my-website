12519cbb1a5e819e7c96bbb00d344b7ce8304fb7
Anirudh Giri <35490486+anirudhgiri@users.noreply.github.com>
2023-03-03 01:23:52 +0530

    Style blog contents and add Title, Subtitle, Name, Read time

--- pkg/routes/v1/blog.go
+++ pkg/routes/v1/blog.go
@@ -1,7 +1,9 @@
 package routes
 
 import (
+	"math"
 	"my-website/pkg/db"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gomarkdown/markdown"
@@ -11,14 +13,32 @@ import (
 func BlogRouter(router fiber.Router) {
 	router.Get("/", func(c *fiber.Ctx) error {
 		blogs, _ := db.GetAllBlogs()
-		return c.Render("blog_list", fiber.Map{"Title": "Anirudh Giri - Blog", "Viewname": "blog_list", "Blog": true, "Blogs": blogs})
+
+		return c.Render("blog_list", fiber.Map{
+			"Title":    "Anirudh Giri - Blog",
+			"Viewname": "blog_list",
+			"Blog":     true,
+			"Blogs":    blogs,
+		})
 	})
 
 	router.Get("/:slug", func(c *fiber.Ctx) error {
 		slug := c.Params("slug")
 		blog := db.GetBlogBySlug(slug)
+
 		mdParser := parser.NewWithExtensions(parser.HardLineBreak | parser.FencedCode | parser.Strikethrough | parser.Attributes)
 		contentHTML := markdown.ToHTML([]byte(blog.Content), mdParser, nil)
-		return c.Render("blog_content", fiber.Map{"blogContent": string(contentHTML), "Blog": true, "Viewname": "blog_content"}, "layouts/blog_content")
+		readTime := math.Max(1, math.Round(float64(len(strings.Split(string(contentHTML), "")))/250))
+
+		return c.Render("blog_content", fiber.Map{
+			"blogTitle":     blog.Title,
+			"blogSubTitle":  blog.SubTitle,
+			"blogCreatedAt": blog.CreatedAt.Format("Jan 02, 2006"),
+			"blogContent":   string(contentHTML),
+			"Blog":          true,
+			"Viewname":      "blog_content",
+			"blogReadTime":  readTime,
+		},
+			"layouts/blog_content")
 	})
 }
